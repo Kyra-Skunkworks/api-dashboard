5ae7f446d5e2dd370402f25b4cafcbec0bf52d58
Chris Skene <cnskene@gmail.com>
2020-01-17 13:17:30 -0500

    layout page

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,17 @@
-import Header from "./Header";
+import Header from './Header';
 
 const layoutStyle = {
   margin: 20,
   padding: 20,
-  border: "1px solid #DDD"
-  // display: "flex"
+  border: '1px solid #DDD',
+  display: 'flex',
 };
 
-const Layout = props => (
+const Layout = (props) => (
   <div style={layoutStyle}>
-    <Header />
+    <div style={{ minWidth: 200, maxWidth: 200 }}>
+      <Header />
+    </div>
     {props.children}
   </div>
 );
